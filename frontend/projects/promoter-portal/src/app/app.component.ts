327236729d055a909c209c06e586a1911e8e078f
Prateik-Lohani <prateik.lohani@quicko.com>
2025-03-27 15:46:09 +0530

    removed: removed global and component store; moved login and signup components. add: created different endpoints for sheet json generated objects in library

--- frontend/projects/promoter-portal/src/app/app.component.ts
+++ frontend/projects/promoter-portal/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProgramStore } from './store/program.store';
+import { ThemeService, ColorUtil } from '@org.quicko.cliq/ngx-core';
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	imports: [RouterOutlet,],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'promoter-portal';
+export class AppComponent implements OnInit {
+
+	readonly programStore = inject(ProgramStore);
+
+	readonly themeService = inject(ThemeService);
+
+	title = computed(() => this.programStore.program()?.name);
+
+	constructor() { }
+
+	ngOnInit() {
+		this.themeService.initializeTheme();
+		ColorUtil.setThemeFromSeed(`#4D5C92`);
+	}
 }
--- frontend/projects/promoter-portal/src/store/app.selector.ts
+++
@@ -1,7 +0,0 @@
-import { createFeatureSelector } from "@ngrx/store";
-import { MemberStoreModel } from "./member-store/member-store.model";
-import { RootKeys } from "./app.model";
-
-export const RootSelectors = {
-	MEMBER: createFeatureSelector<MemberStoreModel>(RootKeys.member_store),
-};
