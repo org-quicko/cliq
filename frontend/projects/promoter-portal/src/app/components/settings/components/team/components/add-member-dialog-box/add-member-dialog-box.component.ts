e6ad0c12c3dba64f9546a952bf36a483e6133e05
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-05 13:43:03 +0530

    Add: add-member dialog box and member-added-success dialog box

---
+++ frontend/projects/promoter-portal/src/app/components/settings/components/team/components/add-member-dialog-box/add-member-dialog-box.component.ts
@@ -0,0 +1,84 @@
+import { TitleCasePipe } from '@angular/common';
+import { Component, computed, effect, inject, Inject, OnDestroy, Signal } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CreateMemberDto, memberRoleEnum, Status } from '@org.quicko.cliq/ngx-core';
+import { RxFormBuilder } from '@rxweb/reactive-form-validators';
+import { Subject } from 'rxjs';
+import { MemberAddedSuccessDialogBoxComponent } from '../member-added-success-dialog-box/member-added-success-dialog-box.component';
+import { MatButtonModule } from '@angular/material/button';
+import { TeamStore } from '../../store/team.store';
+
+@Component({
+	selector: 'app-add-member-dialog-box',
+	imports: [
+		MatDialogModule,
+		ReactiveFormsModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatButtonModule,
+		MatSelectModule,
+		MatInputModule,
+		TitleCasePipe,
+		MemberAddedSuccessDialogBoxComponent
+	],
+	templateUrl: './add-member-dialog-box.component.html',
+	styleUrl: './add-member-dialog-box.component.scss'
+})
+export class AddMemberDialogBoxComponent implements OnDestroy {
+
+	destroy$ = new Subject<boolean>();
+
+	newMember: CreateMemberDto;
+
+	addMemberForm: FormGroup;
+
+	roles = Object.values(memberRoleEnum);
+
+	readonly dialog = inject(MatDialog);
+
+	hidePassword = true;
+
+	constructor(
+		private fb: RxFormBuilder,
+		@Inject(MAT_DIALOG_DATA) public data: { addMember: Function, status: Signal<Status>, setSuccessStatus: Function },
+		private dialogRef: MatDialogRef<AddMemberDialogBoxComponent>
+	) {
+		this.newMember = new CreateMemberDto();
+		this.addMemberForm = this.fb.formGroup(this.newMember);
+
+		effect(() => {
+			if (this.data.status() === Status.SUCCESS) {
+				this.dialog.open(MemberAddedSuccessDialogBoxComponent, {
+					data: {
+						email: this.newMember.email,
+						password: this.newMember.password,
+					}
+				});
+			}
+		});
+	}
+
+
+	onSubmit() {
+		if (this.addMemberForm.valid) {
+			this.data.addMember(this.newMember);
+		}
+	}
+
+	closeDialog() {
+		this.dialogRef.close();
+		this.data.setSuccessStatus();
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next(true);
+		this.destroy$.complete();
+	}
+
+}
---
+++ frontend/projects/promoter-portal/src/app/components/settings/components/team/components/member-added-success-dialog-box/member-added-success-dialog-box.component.ts
@@ -0,0 +1,51 @@
+import { Component, Inject, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Subject } from 'rxjs';
+
+@Component({
+	selector: 'app-member-added-success-dialog-box',
+	imports: [ReactiveFormsModule, MatDialogModule, MatIconModule, MatButtonModule, MatFormFieldModule, FormsModule, MatInputModule],
+	templateUrl: './member-added-success-dialog-box.component.html',
+	styleUrl: './member-added-success-dialog-box.component.scss'
+})
+export class MemberAddedSuccessDialogBoxComponent implements OnDestroy {
+
+	destroy$ = new Subject<boolean>();
+
+	hidePassword = true;
+
+	copyMemberDetails: FormGroup;
+
+	constructor(
+		@Inject(MAT_DIALOG_DATA) public data: { email: string, password: string },
+		private dialogRef: MatDialogRef<MemberAddedSuccessDialogBoxComponent>
+	) {
+
+		this.copyMemberDetails = new FormGroup({
+			email: new FormControl(this.data.email, { nonNullable: true }),
+			password: new FormControl(this.data.password, { nonNullable: true }),
+		});
+		this.copyMemberDetails.disable();
+	}
+
+	closeDialog() {
+		this.dialogRef.close();
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next(true);
+		this.destroy$.complete();
+	}
+
+	onSubmit() {
+		const copyText = `Email: ${this.data.email}\nPassword: ${this.data.password}`;
+		navigator.clipboard.writeText(copyText);
+		this.dialogRef.close(true);
+	}
+
+}
