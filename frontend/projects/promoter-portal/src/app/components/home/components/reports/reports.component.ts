127d2ea053b16d912a6a8b6440c33a3775bb1d68
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-05 13:35:12 +0530

    Change: styling changes and refactored component code

--- frontend/projects/promoter-portal/src/app/components/home/components/reports/reports.component.ts
+++ frontend/projects/promoter-portal/src/app/components/home/components/reports/reports.component.ts
@@ -7,6 +7,7 @@ import { TitleCasePipe } from '@angular/common';
 import { MatRippleModule } from '@angular/material/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ReportDialogBoxComponent } from './report-dialog-box/report-dialog-box.component';
+import { reportEnum } from '@org.quicko.cliq/ngx-core';
 
 @Component({
 	selector: 'app-reports',
@@ -23,18 +24,24 @@ export class ReportsComponent {
 
 	readonly dialog = inject(MatDialog);
 
-	reports = [
-		['purchases', 'local_mall'],
-		['signups', 'group_add'],
-		['commissions', 'supervised_user_circle'],
-	];
+	reportsMap = new Map<reportEnum, string>([
+		[reportEnum.PURCHASES, 'local_mall'],
+		[reportEnum.SIGNUPS, 'group_add'],
+		[reportEnum.COMMISSIONS, 'supervised_user_circle'],
+	]);
 
-	onClickReport(reportName: string) {
-		this.dialog.open(ReportDialogBoxComponent, { data: { reportName } });
-	}
+	onClickReport(reportName: reportEnum) {
+		console.log('clicked!');
 
-	onDownloadCommissionsReport() {
-		this.reportsStore.getReport();
+		this.reportsStore.resetStatus();
+
+		this.dialog.open(ReportDialogBoxComponent, {
+			data: {
+				reportName,
+				getReport: this.reportsStore.getReport,
+				status: this.reportsStore.status
+			}
+		});
 	}
 
 }
