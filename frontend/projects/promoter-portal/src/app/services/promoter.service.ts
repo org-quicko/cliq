4f06a331a76567fa80b4465f29c9d701cd626702
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-05 13:51:06 +0530

    feat: implement promoter management methods including update, remove member, and delete promoter

--- frontend/projects/promoter-portal/src/app/services/promoter.service.ts
+++ frontend/projects/promoter-portal/src/app/services/promoter.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient, HttpResponse } from "@angular/common/http";
-import { computed, inject, Injectable } from "@angular/core";
+import { computed, inject, Injectable, Injector } from "@angular/core";
+import { map } from "rxjs";
+import { ApiResponse, conversionTypeEnum, CreateMemberDto, memberSortByEnum, referralSortByEnum, reportEnum, reportPeriodEnum, sortOrderEnum, statusEnum, UpdatePromoterDto, UpdatePromoterMemberDto } from "@org.quicko.cliq/ngx-core";
+import { instanceToPlain } from "class-transformer";
 import { AuthService } from "./auth.service";
 import { ProgramStore } from "../store/program.store";
 import { PromoterStore } from "../store/promoter.store";
 import { environment } from "../../../environments/environment.dev";
-import { ApiResponse, conversionTypeEnum } from "@org.quicko.cliq/ngx-core";
-import { reportPeriodEnum } from "../../../../org-quicko-cliq-core/src/lib/enums/reportPeriod.enum";
-import { map } from "rxjs";
 
 @Injectable({
 	providedIn: 'root'
@@ -15,16 +15,22 @@ export class PromoterService {
 
 	readonly programStore = inject(ProgramStore);
 
-	readonly promoterStore = inject(PromoterStore);
+	// this is for lazy injection of promoter store -> this is to break the circular dependency of the store and service on each other
+	private injector = inject(Injector);
 
 	constructor(private httpClient: HttpClient, private authService: AuthService) { }
 
 	private endpoint = computed(() => {
-		const promoter = this.promoterStore.promoter();
 		const program = this.programStore.program();
+		const promoter = this.getPromoter();
 
-		return (promoter && program) ? `${environment.base_url}/programs/${program.programId}/promoters/${promoter.promoterId}` : null;
+		return (promoter && program) ? `${environment.base_api_url}/programs/${program.programId}/promoters/${promoter.promoterId}` : null;
 	});
+
+	private getPromoter() {
+		const promoterStore = this.injector.get(PromoterStore); // Lazily get PromoterStore
+		return promoterStore.promoter();
+	}
 
 	getPromoterStatistics() {
 		const url = this.getEndpoint() + '/stats';
@@ -37,40 +43,99 @@ export class PromoterService {
 		});
 	}
 
-	getPromoterCommissions(linkId?: string, conversionType?: conversionTypeEnum) {
-		const url = this.getEndpoint() + '/commissions';
+	updatePromoterInfo(updatedInfo: UpdatePromoterDto) {
+		const url = this.getEndpoint();
 
-		return this.httpClient.get<ApiResponse<any>>(url, {
+		const body = instanceToPlain(updatedInfo);
+
+		return this.httpClient.patch<ApiResponse<any>>(url, body, {
 			headers: {
 				Authorization: this.authService.getToken(),
-				'x-accept-type': 'application/json;format=sheet-json'
 			}
 		});
 	}
 
-	getPromoterReferrals(skip: number = 0, take: number = 4) {
+	removeMember(memberId: string) {
+		const url = this.getEndpoint() + `/members/${memberId}`;
+
+		return this.httpClient.patch<ApiResponse<any>>(url, {}, {
+			headers: {
+				Authorization: this.authService.getToken(),
+			}
+		});
+
+	}
+
+	updateMemberRole(memberId: string, updatedInfo: UpdatePromoterMemberDto) {
+		const url = this.getEndpoint() + `/members/${memberId}/role`;
+
+		const body = instanceToPlain(updatedInfo);
+
+		return this.httpClient.patch<ApiResponse<any>>(url, body, {
+			headers: {
+				Authorization: this.authService.getToken(),
+			}
+		});
+	}
+
+	getPromoterCommissions(
+		queryParams: { link_id?: string, contact_id?: string, conversion_type?: conversionTypeEnum, skip?: number, take?: number }) {
+		const url = this.getEndpoint() + '/commissions';
+
+		if (!queryParams.skip) queryParams.skip = 0;
+		if (!queryParams.take) queryParams.take = 5;
+		if (!queryParams.link_id) delete queryParams.link_id;
+		if (!queryParams.contact_id) delete queryParams.contact_id;
+		if (!queryParams.conversion_type) delete queryParams.conversion_type;
+
+		return this.httpClient.get<ApiResponse<any>>(url, {
+			headers: {
+				Authorization: this.authService.getToken(),
+				'x-accept-type': 'application/json;format=sheet-json',
+			},
+			params: queryParams
+		});
+	}
+
+	getPromoterReferrals(queryParams: { sort_by?: referralSortByEnum, sort_order?: sortOrderEnum, skip?: number, take?: number }) {
 		const url = this.getEndpoint() + '/referrals';
+
+		if (!queryParams.skip) queryParams.skip = 0;
+		if (!queryParams.take) queryParams.take = 5;
+		if (!queryParams.sort_by) delete queryParams.sort_by;
+		if (!queryParams.sort_order) delete queryParams.sort_order;
 
 		return this.httpClient.get<ApiResponse<any>>(url, {
 			headers: {
 				Authorization: this.authService.getToken(),
 				'x-accept-type': 'application/json;format=sheet-json'
 			},
-			params: {
-				skip,
-				take
-			}
+			params: queryParams
 		});
 	}
 
-	getReport(reportPeriod?: reportPeriodEnum, startDate?: Date, endDate?: Date) {
-		const url = this.getEndpoint() + '/reports/commissions';
+	getPromoterReferral(contactId: string) {
+		const url = this.getEndpoint() + `/referrals/${contactId}`;
+
+		return this.httpClient.get<ApiResponse<any>>(url, {
+			headers: {
+				Authorization: this.authService.getToken(),
+			},
+		});
+	}
+
+	getReport(
+		reportType: reportEnum,
+		queryParams: { report_period?: reportPeriodEnum, start_date?: string, end_date?: string }
+	) {
+		const url = this.getEndpoint() + `/reports/${reportType}`;
 
 		return this.httpClient.get(url, {
 			headers: {
 				Authorization: this.authService.getToken(),
 				'x-accept-type': 'application/json;format=sheet-json'
 			},
+			params: queryParams,
 			responseType: 'blob',
 			observe: 'response'
 		}).pipe(
@@ -96,10 +161,54 @@ export class PromoterService {
 		)
 	}
 
+	getAllMembers(
+		queryParams: { sort_by?: memberSortByEnum, sort_order?: sortOrderEnum, skip?: number, take?: number }
+	) {
+		const url = this.getEndpoint() + `/members`;
+
+		if (!queryParams.skip) queryParams.skip = 0;
+		if (!queryParams.take) queryParams.take = 5;
+		if (!queryParams.sort_by) delete queryParams.sort_by;
+		if (!queryParams.sort_order) delete queryParams.sort_order;
+
+		return this.httpClient.get<ApiResponse<any>>(url, {
+			headers: {
+				Authorization: this.authService.getToken(),
+				'x-accept-type': 'application/json;format=sheet-json'
+			},
+			params: {
+				...queryParams,
+				status: statusEnum.ACTIVE,
+			}
+		})
+	}
+
+	addMember(member: CreateMemberDto) {
+		const url = this.getEndpoint() + `/members`;
+
+		const body = instanceToPlain(member);
+
+		return this.httpClient.post<ApiResponse<any>>(url, body, {
+			headers: {
+				Authorization: this.authService.getToken(),
+			}
+		});
+	}
+
+	deletePromoter() {
+		const url = this.getEndpoint();
+
+		return this.httpClient.delete<ApiResponse<any>>(url, {
+			headers: {
+				Authorization: this.authService.getToken(),
+			}
+		});
+	}
+
 	private getEndpoint(): string {
 		const endpoint = this.endpoint();
 		if (!endpoint) {
-			console.log(endpoint);
+			console.error(`Error. Failed to load endpoint for program`, endpoint);
 			throw new Error(`Error. Failed to load endpoint for program`);
 		}
 		return endpoint;
--- frontend/projects/promoter-portal/src/app/store/promoter.store.ts
+++ frontend/projects/promoter-portal/src/app/store/promoter.store.ts
@@ -1,9 +1,18 @@
+import { withDevtools } from "@angular-architects/ngrx-toolkit";
+import { HttpErrorResponse } from "@angular/common/http";
+import { inject } from "@angular/core";
 import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
-import { PromoterDto as Promoter, Status } from "@org.quicko.cliq/ngx-core";
+import { rxMethod } from "@ngrx/signals/rxjs-interop";
+import { PromoterDto as Promoter, PromoterDto, Status, UpdatePromoterDto } from "@org.quicko.cliq/ngx-core";
+import { pipe, switchMap, tap } from "rxjs";
+import { PromoterService } from "../services/promoter.service";
+import { tapResponse } from "@ngrx/operators";
+import { SnackbarService } from "@org.quicko/ngx-core";
+import { plainToInstance } from "class-transformer";
 
 export interface PromoterStoreState {
 	promoter: Promoter | null,
-	error: any | null,
+	error: any,
 	status: Status
 };
 
@@ -17,15 +26,86 @@ export const PromoterStore = signalStore(
 	{ providedIn: 'root' },
 
 	withState(initialPromoterState),
-
+	withDevtools('promoter'),
 	withMethods(
-		(store) => ({
-		setPromoter: (promoter: Promoter) => {
-			patchState(store, { promoter, status: Status.SUCCESS });
-		},
+		(
+			store,
+			promoterService = inject(PromoterService),
+			snackBarService = inject(SnackbarService),
+		) => ({
+			setPromoter: (promoter: Promoter) => {
+				patchState(store, { promoter, status: Status.SUCCESS });
+			},
 
-		setStatus: (status: Status) => {
-			patchState(store, { status });
-		},
-	})),
+			setStatus: (status: Status) => {
+				patchState(store, { status });
+			},
+
+			updatePromoterInfo: rxMethod<UpdatePromoterDto>(
+				pipe(
+					tap(() => patchState(store, { status: Status.LOADING })),
+
+					switchMap((updatedInfo) => {
+						return promoterService.updatePromoterInfo(updatedInfo).pipe(
+							tapResponse({
+								next(response) {
+									const promoterDto = plainToInstance(PromoterDto, response.data);
+
+									patchState(store, { status: Status.SUCCESS, promoter: promoterDto, error: null });
+									snackBarService.openSnackBar('Successfully updated promoter info!', '');
+								},
+								error(error: HttpErrorResponse) {
+									patchState(store, { status: Status.ERROR, error });
+									snackBarService.openSnackBar('Failed to update promoter info', '');
+								},
+							})
+						)
+					})
+				)
+			),
+
+			removeMember: rxMethod<{ memberId: string }>(
+				pipe(
+					tap(() => patchState(store, { status: Status.LOADING })),
+
+					switchMap(({ memberId }) => {
+						return promoterService.removeMember(memberId).pipe(
+							tapResponse({
+								next(response) {
+									console.log(response.data);
+									snackBarService.openSnackBar('Successfully removed member!', '');
+								},
+								error(error: HttpErrorResponse) {
+									patchState(store, { status: Status.ERROR, error });
+									snackBarService.openSnackBar('Failed to remove member', '');
+								},
+							})
+						)
+					})
+				)
+			),
+
+			deletePromoter: rxMethod<void>(
+				pipe(
+					switchMap(() => {
+						return promoterService.deletePromoter().pipe(
+							tapResponse({
+								next(response) {
+									console.log(response);
+									snackBarService.openSnackBar('Successfully deleted promoter', '');
+								},
+								error(error: Error) {
+									if (error instanceof HttpErrorResponse) {
+										console.error(error.error);
+										patchState(store, { status: Status.ERROR, error });
+										snackBarService.openSnackBar('Failed to delete promoter', '');
+									}
+								},
+							})
+						)
+					})
+				)
+			)
+
+		})),
 );
