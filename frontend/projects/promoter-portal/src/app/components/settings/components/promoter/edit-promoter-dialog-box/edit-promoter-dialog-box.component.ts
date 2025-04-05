ef09b7d0a5020da4bd7f5f26f5a7f4851242c75b
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-05 13:41:53 +0530

    Add: edit promoter dialog box

---
+++ frontend/projects/promoter-portal/src/app/components/settings/components/promoter/edit-promoter-dialog-box/edit-promoter-dialog-box.component.ts
@@ -0,0 +1,78 @@
+import { Component, computed, effect, Inject, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Subject } from 'rxjs';
+import { RxFormBuilder } from '@rxweb/reactive-form-validators';
+import { Status, UpdatePromoterDto } from '@org.quicko.cliq/ngx-core';
+import { PromoterStore } from '../../../../../store/promoter.store';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+	selector: 'app-edit-promoter-dialog-box',
+	imports: [
+		FormsModule,
+		ReactiveFormsModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatDialogModule,
+		MatIconModule,
+		MatInputModule,
+		MatProgressSpinnerModule
+	],
+	templateUrl: './edit-promoter-dialog-box.component.html',
+	styleUrl: './edit-promoter-dialog-box.component.scss'
+})
+export class EditPromoterDialogBoxComponent implements OnInit, OnDestroy {
+	destroy$ = new Subject<boolean>();
+
+	refValUniqueCode: string;
+
+	readonly promoterStore = inject(PromoterStore);
+
+	readonly promoter = computed(() => this.promoterStore.promoter());
+
+	promoterDetails: UpdatePromoterDto;
+
+	editPromoterForm: FormGroup;
+
+	hidePassword: boolean = true;
+
+	readonly isLoading = computed(() => this.promoterStore.status() === Status.LOADING);
+
+	constructor(
+		private fb: RxFormBuilder,
+		private dialogRef: MatDialogRef<EditPromoterDialogBoxComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: any,
+	) {
+		effect(() => {
+			this.promoterDetails = new UpdatePromoterDto();
+			this.promoterDetails.name = this.promoter()?.name;
+			this.promoterDetails.logoUrl = this.promoter()?.logoUrl;
+
+			this.editPromoterForm = this.fb.formGroup(this.promoterDetails);
+		});
+	}
+
+	closeDialog(): void {
+		this.dialogRef.close();
+	}
+
+	ngOnInit(): void {
+
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next(true);
+		this.destroy$.complete();
+	}
+
+	onSubmit() {
+		if (this.editPromoterForm.valid) {
+			this.promoterStore.updatePromoterInfo(this.promoterDetails);
+		}
+	}
+}
