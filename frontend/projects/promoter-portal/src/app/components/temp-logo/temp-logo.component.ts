f8d60ce736ff06c9d6d1f9326559d467b3205d03
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-05 13:43:19 +0530

    Add: Temp logo component

---
+++ frontend/projects/promoter-portal/src/app/components/temp-logo/temp-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-temp-logo',
+	imports: [],
+	templateUrl: './temp-logo.component.html',
+	styleUrl: './temp-logo.component.scss'
+})
+export class TempLogoComponent {
+
+}
