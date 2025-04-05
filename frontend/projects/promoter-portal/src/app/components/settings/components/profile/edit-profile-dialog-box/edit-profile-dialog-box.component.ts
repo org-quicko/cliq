83ac40e24f41e2579c9cf7d72c3ccbd4e1bbd7ac
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-05 13:41:22 +0530

    Add: edit profile dialog box

---
+++ frontend/projects/promoter-portal/src/app/components/settings/components/profile/edit-profile-dialog-box/edit-profile-dialog-box.component.ts
@@ -0,0 +1,106 @@
+import { Component, computed, effect, inject, Inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Subject } from 'rxjs';
+import { Status, UpdateMemberDto } from '@org.quicko.cliq/ngx-core';
+import { MatButtonModule } from '@angular/material/button';
+import { MemberStore } from '../../../../../store/member.store';
+import { RxFormBuilder } from '@rxweb/reactive-form-validators';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+	selector: 'app-edit-profile-dialog-box',
+	imports: [
+		MatDialogModule,
+		ReactiveFormsModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatButtonModule,
+		MatInputModule,
+		MatProgressSpinnerModule,
+		MatError
+	],
+	templateUrl: './edit-profile-dialog-box.component.html',
+	styleUrl: './edit-profile-dialog-box.component.scss'
+})
+export class EditProfileDialogBoxComponent implements OnInit, OnDestroy {
+
+	destroy$ = new Subject<boolean>();
+
+	refValUniqueCode: string;
+
+	readonly memberStore = inject(MemberStore);
+
+	readonly member = computed(() => this.memberStore.member());
+
+	profileDetails: UpdateMemberDto;
+
+	editProfileForm: FormGroup;
+
+	hideCurrentPassword: boolean = true;
+
+	hideNewPassword: boolean = true;
+
+	error = computed(() => this.memberStore.error());
+
+	isLoading = computed(() => this.memberStore.status() === Status.LOADING);
+
+	constructor(
+		private fb: RxFormBuilder,
+		private dialogRef: MatDialogRef<EditProfileDialogBoxComponent>,
+		@Inject(MAT_DIALOG_DATA) public data: any,
+	) {
+		effect(() => {
+			this.profileDetails = new UpdateMemberDto();
+			this.profileDetails.email = this.member()?.email ?? '';
+			this.profileDetails.firstName = this.member()?.firstName ?? '';
+			this.profileDetails.lastName = this.member()?.lastName ?? '';
+
+			this.editProfileForm = this.fb.formGroup(this.profileDetails);
+
+			this.editProfileForm.addValidators(this.passwordMatchValidator);
+		});
+	}
+
+	closeDialog(): void {
+		this.dialogRef.close();
+	}
+
+	ngOnInit(): void {
+
+	}
+
+	ngOnDestroy(): void {
+		this.destroy$.next(true);
+		this.destroy$.complete();
+	}
+
+	onSubmit() {
+		if (this.editProfileForm.valid) {
+			this.memberStore.updateMemberInfo(this.profileDetails);
+		}
+	}
+
+	passwordMatchValidator = (): { [key: string]: boolean } | null => {
+		const currentPassword = this.editProfileForm?.get('currentPassword')?.value;
+		const newPassword = this.editProfileForm?.get('newPassword')?.value;
+
+		if (currentPassword || newPassword) {
+			if (!currentPassword || !newPassword) {
+				return { passwordsRequired: true };
+			}
+
+			if (currentPassword === newPassword) {
+				return { passwordsMatch: true };
+			}
+		}
+
+		return null;
+	};
+
+
+}
