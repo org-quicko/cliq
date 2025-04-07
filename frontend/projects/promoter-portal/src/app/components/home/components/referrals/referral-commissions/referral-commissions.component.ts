1bcf8ca51839e0b4ff6082c81c42ae519ea674cc
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-07 20:40:38 +0530

    Add: added pagination

--- frontend/projects/promoter-portal/src/app/components/home/components/referrals/referral-commissions/referral-commissions.component.ts
+++ frontend/projects/promoter-portal/src/app/components/home/components/referrals/referral-commissions/referral-commissions.component.ts
@@ -1,16 +1,18 @@
-import { Component, computed, effect, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, computed, effect, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ReferralCommissionsStore } from './store/referralCommissions.store';
 import { CommissionRow } from '@org.quicko.cliq/ngx-core/generated/sources/Promoter';
-import { FormatCurrencyPipe, OrdinalDatePipe } from '@org.quicko.cliq/ngx-core';
+import { FormatCurrencyPipe, OrdinalDatePipe, PaginationOptions, referralSortByEnum, sortOrderEnum } from '@org.quicko.cliq/ngx-core';
 import { MatButtonModule } from '@angular/material/button';
 import { ProgramStore } from '../../../../../store/program.store';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { SkeletonLoadTableComponent } from '../../../../common/skeleton-load-table/skeleton-load-table.component';
 
 @Component({
 	selector: 'app-referral-commissions',
@@ -21,9 +23,11 @@ import { ProgramStore } from '../../../../../store/program.store';
 		MatSortModule,
 		MatChipsModule,
 		MatRippleModule,
+		MatPaginatorModule,
 		TitleCasePipe,
 		OrdinalDatePipe,
 		FormatCurrencyPipe,
+		SkeletonLoadTableComponent,
 	],
 	providers: [ReferralCommissionsStore],
 	templateUrl: './referral-commissions.component.html',
@@ -31,6 +35,7 @@ import { ProgramStore } from '../../../../../store/program.store';
 })
 export class ReferralCommissionsComponent implements OnInit {
 	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
 	displayedColumns: string[] = ['created at', 'type', 'commission', 'revenue'];
 
@@ -44,22 +49,79 @@ export class ReferralCommissionsComponent implements OnInit {
 
 	readonly currency = computed(() => this.programStore.program()?.currency);
 
+	sortOptions = signal<{ active: referralSortByEnum, direction: 'asc' | 'desc' }>({
+		active: referralSortByEnum.UPDATED_AT,
+		direction: 'asc',
+	});
+
+	paginationOptions = signal<PaginationOptions>({
+		pageIndex: 0,
+		pageSize: 5,
+	});
+
+	readonly totalDataLength = computed(() => {
+		const metadata = this.referralCommissionsStore.commissions()?.getMetadata();
+		const count = metadata ? metadata.get('count') : null;
+		return count ? Number(count) : 0; // Returns 0 if count is undefined
+	});
+
 	constructor(private router: Router, private route: ActivatedRoute) {
 		effect(() => {
 			const referralCommissions = (this.referralCommissionsStore.commissions()?.getRows() ?? []) as CommissionRow[];
-			this.dataSource.data = referralCommissions;
+
+			const { pageIndex, pageSize } = this.paginationOptions();
+
+			const start = pageIndex * pageSize;
+			const end = Math.min(start + pageSize, referralCommissions.length);
+
+			this.dataSource.data = referralCommissions.slice(start, end);
 		});
 	}
 
 	ngOnInit(): void {
+		this.referralCommissionsStore.resetLoadedPages();
 		const contactId = this.route.snapshot.paramMap.get('contact_id');
+		
 		if (contactId) {
-			this.referralCommissionsStore.getReferralCommissions({ contactId });
+			this.loadReferralCommissions();
 			this.referralCommissionsStore.getReferral({ contactId });
 		} else {
 			console.error('No contact_id found in route.');
 		}
 	}
+
+	loadReferralCommissions(isSorting: boolean = false) {
+		const { pageIndex, pageSize } = this.paginationOptions();
+		const skip = pageIndex * pageSize;
+
+		const contactId = this.route.snapshot.paramMap.get('contact_id');
+		if (contactId) {
+			this.referralCommissionsStore.getReferralCommissions({
+				contactId,
+				sortBy: this.sortOptions().active,
+				sortOrder: this.sortOptions().direction === 'asc' ? sortOrderEnum.ASCENDING : sortOrderEnum.DESCENDING,
+				skip,
+				take: pageSize,
+				isSorting
+			});
+		} else {
+			console.error('No contact_id found in route.');
+		}
+	}
+
+	onSortChange(event: Sort) {
+		this.paginationOptions.set({ pageSize: 5, pageIndex: 0 });
+		this.referralCommissionsStore.resetLoadedPages();
+		this.sortOptions.set({ active: event.active as referralSortByEnum, direction: event.direction as 'asc' | 'desc' });
+
+		this.loadReferralCommissions(true);
+	}
+
+	onPageChange(event: PageEvent) {
+		this.paginationOptions.set({ pageIndex: event.pageIndex, pageSize: event.pageSize });
+		this.loadReferralCommissions();
+	}
+
 
 	onGoBack() {
 		this.router.navigate([`../../../`], { relativeTo: this.route });
--- frontend/projects/promoter-portal/src/app/components/home/components/referrals/referral-commissions/store/referralCommissions.store.ts
+++ frontend/projects/promoter-portal/src/app/components/home/components/referrals/referral-commissions/store/referralCommissions.store.ts
@@ -1,27 +1,29 @@
 import { inject } from "@angular/core";
 import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
 import { rxMethod } from "@ngrx/signals/rxjs-interop";
-import { pipe, switchMap, tap } from "rxjs";
+import { of, pipe, switchMap, tap } from "rxjs";
 import { tapResponse } from "@ngrx/operators";
 import { plainToInstance } from "class-transformer";
-import { Status, SnackbarService, ContactDto, ReferralDto } from "@org.quicko.cliq/ngx-core";
-import { CommissionTable, PromoterWorkbook } from "@org.quicko.cliq/ngx-core/generated/sources/Promoter";
+import { Status, SnackbarService, ContactDto, ReferralDto, sortOrderEnum, referralSortByEnum } from "@org.quicko.cliq/ngx-core";
+import { CommissionRow, CommissionTable, PromoterWorkbook } from "@org.quicko.cliq/ngx-core/generated/sources/Promoter";
 import { PromoterService } from "../../../../../../services/promoter.service";
 import { withDevtools } from "@angular-architects/ngrx-toolkit";
 import { HttpErrorResponse } from "@angular/common/http";
 
 export interface ReferralCommissionsStoreState {
-	commissions: CommissionTable | null,
-	contact: ReferralDto | null,
-	error: any | null,
-	status: Status
+	commissions: CommissionTable | null;
+	contact: ReferralDto | null;
+	error: any | null;
+	status: Status;
+	loadedPages: Set<number>;
 };
 
 export const initialCommissionState: ReferralCommissionsStoreState = {
 	commissions: null,
 	contact: null,
 	error: null,
-	status: Status.PENDING
+	status: Status.PENDING,
+	loadedPages: new Set<number>()
 };
 
 export const ReferralCommissionsStore = signalStore(
@@ -33,17 +35,50 @@ export const ReferralCommissionsStore = signalStore(
 			promoterService = inject(PromoterService),
 			snackBarService = inject(SnackbarService)
 		) => ({
-			getReferralCommissions: rxMethod<{ contactId: string, skip?: number, take?: number }>(
+			getReferralCommissions: rxMethod<{ sortOrder?: sortOrderEnum, sortBy?: referralSortByEnum, contactId: string, skip?: number, take?: number, isSorting?: boolean }>(
 				pipe(
-					tap(() => patchState(store, { status: Status.LOADING })),
+					tap(({ isSorting }) => {
+						if (!isSorting) {
+							patchState(store, { status: Status.LOADING });
+						}
+					}),
 
-					switchMap(({ contactId, skip, take }) => {
-						return promoterService.getPromoterCommissions({ contact_id: contactId, skip, take }).pipe(
+					switchMap(({ sortBy, sortOrder, contactId, skip, take, isSorting }) => {
+
+						const page = Math.floor((skip ?? 0) / (take ?? 5));
+
+						if (!isSorting && store.loadedPages().has(page)) {
+							patchState(store, { status: Status.SUCCESS });
+							return of(store.commissions()); // ✅ skip request if page already loaded
+						}
+
+						return promoterService.getPromoterCommissions({ sort_by: sortBy, sort_order: sortOrder, contact_id: contactId, skip, take }).pipe(
 							tapResponse({
 								next: (response) => {
+									const commissionTable = plainToInstance(PromoterWorkbook, response.data).getCommissionSheet().getCommissionTable();
+
+									let currentCommissionTable = store.commissions();
+
+									const updatedPages = store.loadedPages().add(page);
+
+									// append entries in case new entries incoming
+									if (currentCommissionTable && !isSorting) {
+										const rows = commissionTable.getRows();
+										for (const row of rows) {
+											const commission = new CommissionRow(row as any[]);
+											currentCommissionTable.addRow(commission);
+										}
+
+									} else {
+										currentCommissionTable = commissionTable;
+									}
+
+									const updatedMemberTable = Object.assign(new CommissionTable(), currentCommissionTable);
+
 									patchState(store, {
-										commissions: plainToInstance(PromoterWorkbook, response.data).getCommissionSheet().getCommissionTable(),
-										status: Status.SUCCESS
+										commissions: updatedMemberTable,
+										status: Status.SUCCESS,
+										loadedPages: updatedPages
 									})
 								},
 								error: (error: HttpErrorResponse) => {
@@ -59,6 +94,10 @@ export const ReferralCommissionsStore = signalStore(
 					})
 				)
 			),
+
+			resetLoadedPages() {
+				patchState(store, { loadedPages: new Set(), commissions: null, contact: null });
+			},
 
 			getReferral: rxMethod<{ contactId: string }>(
 				pipe(
