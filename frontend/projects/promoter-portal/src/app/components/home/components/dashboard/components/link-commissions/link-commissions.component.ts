db9a664b5701ab0137589a060a0d35a47d54d056
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-07 20:36:51 +0530

    Add: added pagination to link commissions store

--- frontend/projects/promoter-portal/src/app/components/home/components/dashboard/components/link-commissions/link-commissions.component.ts
+++ frontend/projects/promoter-portal/src/app/components/home/components/dashboard/components/link-commissions/link-commissions.component.ts
@@ -3,7 +3,7 @@ import { Component, computed, effect, inject, OnInit, signal, ViewChild } from '
 import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,9 +11,12 @@ import { ProgramStore } from '../../../../../../store/program.store';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LinkCommissionsStore } from './store/linkCommission.store';
 import { CommissionRow } from '@org.quicko.cliq/ngx-core/generated/sources/Promoter';
-import { FormatCurrencyPipe, OrdinalDatePipe, Status, ZeroToDashPipe } from '@org.quicko.cliq/ngx-core';
+import { FormatCurrencyPipe, memberSortByEnum, OrdinalDatePipe, PaginationOptions, Status, ZeroToDashPipe } from '@org.quicko.cliq/ngx-core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MemberSortOptions } from '../../../../../../interfaces';
+import { SkeletonLoadTableComponent } from '../../../../../common/skeleton-load-table/skeleton-load-table.component';
 
 @Component({
 	selector: 'app-link-commissions',
@@ -24,13 +27,15 @@ import { MatInputModule } from '@angular/material/input';
 		MatTableModule,
 		FormsModule,
 		MatFormFieldModule,
+		MatPaginatorModule,
 		MatSortModule,
 		MatChipsModule,
 		MatRippleModule,
 		NgClass,
 		TitleCasePipe,
 		OrdinalDatePipe,
-		FormatCurrencyPipe
+		FormatCurrencyPipe,
+		SkeletonLoadTableComponent
 	],
 	providers: [LinkCommissionsStore],
 	templateUrl: './link-commissions.component.html',
@@ -39,47 +44,92 @@ import { MatInputModule } from '@angular/material/input';
 export class LinkCommissionsComponent implements OnInit {
 
 	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
 	displayedColumns: string[] = ['created at', 'referral', 'type', 'commission', 'revenue'];
 
 	dataSource: MatTableDataSource<CommissionRow> = new MatTableDataSource<CommissionRow>([]);
 
-	readonly programStore = inject(ProgramStore);
+	sortOptions = signal<{ active: string, direction: 'asc' | 'desc' }>({
+		active: 'created at',
+		direction: 'desc',
+	});
 
+	paginationOptions = signal<PaginationOptions>({
+		pageIndex: 0,
+		pageSize: 5,
+	});
+
+	readonly programStore = inject(ProgramStore);
 	readonly linkCommissionsStore = inject(LinkCommissionsStore);
 
 	readonly link = computed(() => this.linkCommissionsStore.link());
-
-	fullLinkString = computed(() => this.program()?.website + '?ref=' + this.link()?.refVal);
-
 	readonly program = computed(() => this.programStore.program());
-
-	referralKeyType = computed(() => this.programStore.program()?.referralKeyType);
-
-	referralKey = signal('');
-
+	readonly fullLinkString = computed(() => this.program()?.website + '?ref=' + this.link()?.refVal);
+	readonly referralKeyType = computed(() => this.programStore.program()?.referralKeyType);
 	readonly isLoading = computed(() => this.linkCommissionsStore.status() === Status.LOADING);
 
-	linkCommissionsLength = computed(() => {
-		const commissions = this.linkCommissionsStore.commissions();
-		return commissions ? commissions.length() : 0;
+	readonly totalDataLength = computed(() => {
+		const metadata = this.linkCommissionsStore.commissions()?.getMetadata();
+		const count = metadata ? metadata.get('count') : null;
+		return count ? Number(count) : 0; // Returns 0 if count is undefined
 	});
+
+	referralKey = signal('');
 
 	constructor(private router: Router, private route: ActivatedRoute) {
 		effect(() => {
 			const linkCommissions = (this.linkCommissionsStore.commissions()?.getRows() ?? []) as CommissionRow[];
-			this.dataSource.data = linkCommissions;
+			const { pageIndex, pageSize } = this.paginationOptions();
+
+			const start = pageIndex * pageSize;
+			const end = Math.min(start + pageSize, linkCommissions.length);
+
+			this.dataSource.data = linkCommissions.slice(start, end);
 		});
 	}
 
 	ngOnInit(): void {
+		this.linkCommissionsStore.resetLoadedPages();
+
 		const linkId = this.route.snapshot.paramMap.get('link_id');
 		if (linkId) {
-			this.linkCommissionsStore.getLinkCommissions({ linkId });
+			this.loadLinkCommissions();
 			this.linkCommissionsStore.getLink({ linkId });
 		} else {
 			console.error('No link_id found in route.');
 		}
+	}
+
+	loadLinkCommissions(isSorting: boolean = false) {
+		const { pageIndex, pageSize } = this.paginationOptions();
+		const skip = pageIndex * pageSize;
+
+		const linkId = this.route.snapshot.paramMap.get('link_id');
+
+		if (linkId) {
+			this.linkCommissionsStore.getLinkCommissions({
+				linkId,
+				skip,
+				take: pageSize,
+				isSorting
+			});
+		} else {
+			console.error('No link_id found in route.');
+		}
+	}
+
+	onSortChange(event: Sort) {
+		this.paginationOptions.set({ pageSize: 5, pageIndex: 0 });
+		this.linkCommissionsStore.resetLoadedPages();
+		this.sortOptions.set({ active: event.active, direction: event.direction as 'asc' | 'desc' });
+
+		this.loadLinkCommissions(true);
+	}
+
+	onPageChange(event: PageEvent) {
+		this.paginationOptions.set({ pageIndex: event.pageIndex, pageSize: event.pageSize });
+		this.loadLinkCommissions();
 	}
 
 	onGoBack() {
--- frontend/projects/promoter-portal/src/app/components/home/components/dashboard/components/link-commissions/store/linkCommission.store.ts
+++ frontend/projects/promoter-portal/src/app/components/home/components/dashboard/components/link-commissions/store/linkCommission.store.ts
@@ -1,28 +1,30 @@
 import { inject } from "@angular/core";
 import { patchState, signalStore, withMethods, withState } from "@ngrx/signals";
 import { rxMethod } from "@ngrx/signals/rxjs-interop";
-import { pipe, switchMap, tap } from "rxjs";
+import { of, pipe, switchMap, tap } from "rxjs";
 import { tapResponse } from "@ngrx/operators";
 import { plainToInstance } from "class-transformer";
 import { Status, SnackbarService, LinkDto } from "@org.quicko.cliq/ngx-core";
-import { CommissionTable, PromoterWorkbook } from "@org.quicko.cliq/ngx-core/generated/sources/Promoter";
+import { CommissionRow, CommissionTable, PromoterWorkbook } from "@org.quicko.cliq/ngx-core/generated/sources/Promoter";
 import { PromoterService } from "../../../../../../../services/promoter.service";
 import { withDevtools } from "@angular-architects/ngrx-toolkit";
 import { LinkService } from "../../../../../../../services/link.service";
 import { HttpErrorResponse } from '@angular/common/http';
 
 export interface LinkCommissionsStoreState {
-	commissions: CommissionTable | null,
-	link: LinkDto | null,
-	error: any | null,
-	status: Status,
+	commissions: CommissionTable | null;
+	link: LinkDto | null;
+	error: any | null;
+	status: Status;
+	loadedPages: Set<number>;
 };
 
 export const initialCommissionState: LinkCommissionsStoreState = {
 	commissions: null,
 	link: null,
 	error: null,
-	status: Status.PENDING
+	status: Status.PENDING,
+	loadedPages: new Set<number>()
 };
 
 export const LinkCommissionsStore = signalStore(
@@ -36,18 +38,48 @@ export const LinkCommissionsStore = signalStore(
 			snackBarService = inject(SnackbarService)
 		) => ({
 
-			getLinkCommissions: rxMethod<{ linkId: string, skip?: number, take?: number }>(
+			getLinkCommissions: rxMethod<{ linkId: string, skip?: number, take?: number, isSorting?: boolean }>(
 				pipe(
-					tap(() => patchState(store, { status: Status.LOADING })),
+					tap(({ isSorting }) => {
+						if (!isSorting) {
+							patchState(store, { status: Status.LOADING });
+						}
+					}),
 
-					switchMap(({ linkId, skip, take }) => {
+					switchMap(({ linkId, skip, take, isSorting }) => {
+
+						const page = Math.floor((skip ?? 0) / (take ?? 5));
+
+						if (!isSorting && store.loadedPages().has(page)) {
+							patchState(store, { status: Status.SUCCESS });
+							return of(store.commissions()); // ✅ skip request if page already loaded
+						}
+
+						console.log('here');
 						return promoterService.getPromoterCommissions({ link_id: linkId, skip, take }).pipe(
 							tapResponse({
 								next: (response) => {
-									patchState(store, {
-										commissions: plainToInstance(PromoterWorkbook, response.data).getCommissionSheet().getCommissionTable(),
-										status: Status.SUCCESS
-									});
+									const commissionsTable = plainToInstance(PromoterWorkbook, response.data).getCommissionSheet().getCommissionTable()
+
+									let currentCommissionsTable = store.commissions();
+
+									const updatedPages = store.loadedPages().add(page);
+
+									// append entries in case new entries incoming
+									if (currentCommissionsTable && !isSorting) {
+										const rows = commissionsTable.getRows();
+										for (const row of rows) {
+											const commission = new CommissionRow(row as any[]);
+											currentCommissionsTable.addRow(commission);
+										}
+
+									} else {
+										currentCommissionsTable = commissionsTable;
+									}
+
+									const updatedMemberTable = Object.assign(new CommissionTable(), currentCommissionsTable);
+
+									patchState(store, { commissions: updatedMemberTable, status: Status.SUCCESS, loadedPages: updatedPages });
 								},
 								error: (error: HttpErrorResponse) => {
 									if (error.status == 404) {
@@ -64,6 +96,10 @@ export const LinkCommissionsStore = signalStore(
 					})
 				)
 			),
+
+			resetLoadedPages() {
+				patchState(store, { loadedPages: new Set(), commissions: null, link: null });
+			},
 
 			getLink: rxMethod<{ linkId: string }>(
 				pipe(
