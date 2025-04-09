143735e485485788f6cd60f3080efbb96749db45
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-09 11:25:11 +0530

    Change: program resolver now uses pipe instead of firstValueFrom

--- frontend/projects/promoter-portal/src/app/resolver/program.resolver.ts
+++ frontend/projects/promoter-portal/src/app/resolver/program.resolver.ts
@@ -1,39 +1,44 @@
 import { Injectable, inject } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom, map, Observable, of, tap } from 'rxjs';
 import { ProgramDto as Program, ProgramDto } from '../../../../org-quicko-cliq-core/src/lib/dtos';
 import { ProgramService } from '../services/program.service';
 import { ProgramStore } from '../store/program.store';
 import { Status } from '../../../../org-quicko-cliq-core/src/lib/enums/stateStatus.enum';
 import { plainToInstance } from 'class-transformer';
 import { tapResponse } from '@ngrx/operators';
+import { SnackbarService } from '@org.quicko/ngx-core';
 
 
 @Injectable({ providedIn: 'root' })
 export class ProgramResolver implements Resolve<Program> {
 	constructor() { }
 
-	readonly store = inject(ProgramStore);
+	readonly programStore = inject(ProgramStore);
 
 	readonly programService = inject(ProgramService);
+	readonly snackBarService = inject(SnackbarService);
 
-	async resolve(route: ActivatedRouteSnapshot) {
+	resolve(route: ActivatedRouteSnapshot): Observable<ProgramDto> {
 		const programId = route.paramMap.get('program_id'); // Get program_id from route
 		if (!programId) {
 			throw new Error('Program ID is missing in route');
 		}
 
-		this.store.setStatus(Status.LOADING);
-
-		const response = await firstValueFrom(this.programService.getProgram(programId));
-
-		if (!response.data) {
-			this.store.setStatus(Status.ERROR);
-			throw new Error('Program not found');
-		}
-
-		this.store.setProgram(plainToInstance(ProgramDto, response.data)); // set the global store
-		return response.data;
+		return this.programService.getProgram(programId).pipe(
+			tap((response) => {
+				if (response.data) {
+					const program = plainToInstance(ProgramDto, response.data);
+					this.programStore.setProgram(program);
+				}
+			}),
+			map((response) => plainToInstance(ProgramDto, response.data?.promoter) ?? new ProgramDto()),
+			catchError((error) => {
+				this.snackBarService.openSnackBar('Failed to get program', '');
+				this.programStore.setStatus(Status.ERROR, error);
+				return of(new ProgramDto());
+			})
+		)
 	}
 
 }
