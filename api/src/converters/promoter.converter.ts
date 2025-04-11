b91edc7f244e3eb3ac3816356e13aa1686892f27
Prateik-Lohani <prateik.lohani@quicko.com>
2025-04-11 19:07:06 +0530

    Add: added accepted tnc field to promoter dto converter

--- api/src/converters/promoter.converter.ts
+++ api/src/converters/promoter.converter.ts
@@ -4,13 +4,14 @@ import { Promoter } from '../entities';
 
 @Injectable()
 export class PromoterConverter {
-	public convert(promoter: Promoter): PromoterDto {
+	public convert(promoter: Promoter, acceptedTermsAndConditions: boolean): PromoterDto {
 		const promoterDto = new PromoterDto();
 
 		promoterDto.promoterId = promoter.promoterId;
 
 		promoterDto.name = promoter.name;
 		promoterDto.logoUrl = promoter.logoUrl;
+		promoterDto.acceptedTermsAndConditions = acceptedTermsAndConditions;
 
 		promoterDto.createdAt = new Date(promoter.createdAt);
 		promoterDto.updatedAt = new Date(promoter.updatedAt);
