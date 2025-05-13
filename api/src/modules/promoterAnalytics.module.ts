c4360f61fd26886e8a63506fb618578fe0fa7ead
Prateik-Lohani <prateik.lohani@quicko.com>
2025-05-13 18:55:31 +0530

    fixed: promoter analytics not found issue

--- api/src/modules/promoterAnalytics.module.ts
+++ api/src/modules/promoterAnalytics.module.ts
@@ -2,11 +2,10 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PromoterAnalyticsConverter } from "src/converters/promoterAnalytics.converter";
 import { PromoterAnalyticsView } from "src/entities";
-import { PromoterAnalyticsService } from "src/services/promoterAnalytics.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([PromoterAnalyticsView])],
-    providers: [PromoterAnalyticsService, PromoterAnalyticsConverter],
-    exports: [PromoterAnalyticsService, PromoterAnalyticsConverter]
+    providers: [PromoterAnalyticsConverter],
+    exports: [PromoterAnalyticsConverter]
 })
 export class PromoterAnalyticsModule { }
