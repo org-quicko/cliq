4d79058e33021f87e5ed2f1591e5a368877b5262
Prateik-Lohani <prateik.lohani@quicko.com>
2025-05-13 11:49:51 +0530

    fixed: static file server issue

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -6,8 +6,4 @@ import { join } from 'path';
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) { }
-	@Get()
-	root(@Res() response: Response): void {
-		response.sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
-	}
 }
